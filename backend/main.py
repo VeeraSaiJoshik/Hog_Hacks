b3e8dbd0169d8e2ad4bb86984601095bde039eba
Jhon Perez <juanp20077@gmail.com>
2024-04-12 20:09:22 -0500

    start backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
+from openai import OpenAI
+from dotenv import load_dotenv
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    load_dotenv()
+    uvicorn.run("__main__:app", host="127.0.0.1", port=8000, reload=True)
