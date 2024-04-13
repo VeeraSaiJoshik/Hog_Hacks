11001f8f84284be80b611973d37c840d693965ca
VeeraSaiJoshik <JoshikDevelop@gmail.com>
2024-04-12 20:39:57 -0500

    base ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
-
-class TemporaryPage extends StatefulWidget {
-  const TemporaryPage({super.key});
-
-  @override
-  State<TemporaryPage> createState() => _TemporaryPageState();
-}
-
-class _TemporaryPageState extends State<TemporaryPage> {
-  @override
-  Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Container(
-        child : Text(
-          "Hello Worldasdf"
-        )
-      ),
-    );
-  }
-}
+import 'package:haghocks/pages/authentication.dart';
 
 void main() {
-  runApp(const TemporaryPage());
+  runApp(
+    MaterialApp(
+      home: AuthenticationPage()
+    )
+  );
 }
--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -13,10 +13,38 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Container(
-      height: height, 
-      width: width, 
-      color: Custom_Color.background,
+    return Scaffold(
+        body: Container(
+          height: height, 
+          width: width, 
+          color: Custom_Color.background,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                SizedBox(
+                  height: 200,
+                  width: 300,
+                  child: FittedBox(
+                    child: ElevatedButton(
+                      child: Text("Sign Up"),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 200,
+                  width: 300,
+                  child: FittedBox(
+                    child: ElevatedButton(
+                      child: Text("Log In"),
+                      onPressed: () {},
+                    ),
+                  ),
+                )
+            ],
+          ),
+        ),
     );
   }
 }
