fa18da76ae9c449519a85af29b7d7d21b747dc21
Rockymine8 <jrbowlin7@gmail.com>
2024-04-12 22:19:22 -0500

    authenticate with google

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:haghocks/firebase_options.dart';
 import 'package:haghocks/pages/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: AuthenticationPage()
-    )
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(home: AuthenticationPage()));
 }
--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:haghocks/global_info.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthenticationPage extends StatefulWidget {
   const AuthenticationPage({super.key});
@@ -9,42 +13,56 @@ class AuthenticationPage extends StatefulWidget {
 }
 
 class _AuthenticationPageState extends State<AuthenticationPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((event) {
+      setState(() {
+        _user = event;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-        body: Container(
-          height: height, 
-          width: width, 
-          color: Custom_Color.background,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                SizedBox(
-                  height: 200,
-                  width: 300,
-                  child: FittedBox(
-                    child: ElevatedButton(
-                      child: Text("Sign Up"),
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 200,
-                  width: 300,
-                  child: FittedBox(
-                    child: ElevatedButton(
-                      child: Text("Log In"),
-                      onPressed: () {},
-                    ),
-                  ),
-                )
-            ],
+    return Scaffold(body: _user != null ? _userInfo() : _googleSignInButton());
+  }
+
+  Widget _googleSignInButton() {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+        ), // Add padding to prevent overflow
+        child: SizedBox(
+          height: 50,
+          width: double.infinity, // Ensure button takes full width
+          child: SignInButton(
+            Buttons.apple,
+            text: "Sign up with Google",
+            onPressed: _handleGoogleSignIn,
           ),
         ),
+      ),
     );
+  }
+
+  Widget _userInfo() {
+    return SizedBox();
+  }
+
+  void _handleGoogleSignIn() {
+    try {
+      GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(_googleAuthProvider);
+    } catch (error) {
+      print(error);
+    }
   }
 }
